0d807288318545f79b26a77bdfa5545af156a08e
Justin <justin.n.4rws@gmail.com>
2019-03-18 21:12:34 -0400

    Added simple job name input to time entry table
    Added an input field for job name, added a submit button that calls a simple
    print statement, and made small css changes.

--- src/app/time-entry/time-entry.component.ts
+++ src/app/time-entry/time-entry.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 export class TimeEntryComponent {
 
   constructor() { }
+
+  jobName: string;
+
+  onSubmit() {
+    console.log("Job Name:", this.jobName);
+  }
 }
