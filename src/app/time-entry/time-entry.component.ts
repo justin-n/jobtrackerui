4fad16289a688610676bc1c4efa00846aa0da25d
Justin <justin.n.4rws@gmail.com>
2019-03-23 11:09:12 -0400

    Added drop down list for timeInHour
    Added getHourOptions method, timeInHour instance variable, hourOptions variable, and additional line in onSubmit method to time-entry.component.ts, added select and option tag for timeInHour to time-entry.component.html

--- src/app/time-entry/time-entry.component.ts
+++ src/app/time-entry/time-entry.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Option } from './option';
 
 @Component({
   selector: 'app-time-entry',
@@ -10,8 +11,30 @@ export class TimeEntryComponent {
   constructor() { }
 
   jobName: string;
+  timeInHour: string = "01";
+
+  hourOptions: Option[] = TimeEntryComponent.getHourOptions();
 
   onSubmit() {
     console.log("Job Name:", this.jobName);
+    console.log("timeInHour:", this.timeInHour);
+  }
+
+  private static getHourOptions() : Option[] {
+
+    let hourOptions: Option[] = [];
+
+    for (let hour = 1; hour <= 12; hour++) {
+
+      let hourStringValue = String(hour);
+
+      if (hourStringValue.length == 1) {
+        hourStringValue = ("0" + hourStringValue);
+      }
+
+      hourOptions.push({key: hourStringValue, value: hourStringValue});
+    }
+
+    return hourOptions;
   }
 }
