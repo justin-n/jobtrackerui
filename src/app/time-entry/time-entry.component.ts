7b8bd7c75bbf4ead8dda0996a9b261bb090228e6
Justin <justin.n.4rws@gmail.com>
2019-03-24 11:56:12 -0400

    Added selectors to time entry form
    Added selector for timeInPeriod, timeOutHour, timeOutMinute, timeOutPeriod, and month to time-entry.component.html; added periodOptions and instance variables to time-entry.component.ts

--- src/app/time-entry/time-entry.component.ts
+++ src/app/time-entry/time-entry.component.ts
@@ -10,17 +10,35 @@ export class TimeEntryComponent {
 
   constructor() { }
 
+  static readonly periodOptions: Option[] = [
+    { key: "AM", value: "AM" },
+    { key: "PM", value: "PM" }
+  ];
+
   jobName: string;
   timeInHour: string = "01";
   timeInMinute: string = "00";
+  timeInPeriod: string = "AM";
+  timeOutHour: string = "01";
+  timeOutMinute: string = "00";
+  timeOutPeriod: string = "AM";
+
+  month: string = "01";
 
   hourOptions: Option[] = TimeEntryComponent.getTwoLengthNumStrOptions(1, 12, 1);
   minuteOptions: Option[] = TimeEntryComponent.getTwoLengthNumStrOptions(0, 59, 5);
+  periodOptions: Option[] = TimeEntryComponent.periodOptions;
+  monthOptions: Option[] = TimeEntryComponent.getTwoLengthNumStrOptions(1, 12, 1);
 
   onSubmit() {
     console.log("Job Name:", this.jobName);
     console.log("timeInHour:", this.timeInHour);
     console.log("timeInMinute:", this.timeInMinute);
+    console.log("timeInPeriod:", this.timeInPeriod);
+    console.log("timeOutHour:", this.timeOutHour);
+    console.log("timeOutMinute:", this.timeOutMinute);
+    console.log("timeOutPeriod:", this.timeOutPeriod);
+    console.log("month:", this.month);
   }
 
   private static getTwoLengthNumStrOptions(
